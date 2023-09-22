b958478f8250c98a2a27757f08b59ca38b3f90da
VedantMMahadik <138750808+VedantMMahadik@users.noreply.github.com>
2023-09-22 12:36:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-window.onload = function() {
-    let minutes=0;
-    let seconds=0;
-    let tens=0;
-    let appendMinutes=document.querySelector('#minutes')
-    let appendTens=document.querySelector('#tens')
-    let appendSeconds=document.querySelector('#seconds')
+window.onload = function () {
+    let minutes = 0;
+    let seconds = 0;
+    let tens = 0;
+    let appendMinutes = document.querySelector('#minutes');
+    let appendTens = document.querySelector('#tens');
+    let appendSeconds = document.querySelector('#seconds');
 
-    let startBtn = document.querySelector('#start')
-    let stopBtn = document.querySelector('#stop')
-    let resetBtn = document.querySelector('#reset')
+    let startBtn = document.querySelector('#start');
+    let stopBtn = document.querySelector('#stop');
+    let resetBtn = document.querySelector('#reset');
+    let lapBtn = document.querySelector('#lap'); // Added lap button
+    let lapTimesContainer = document.querySelector('#lap-times'); // Added lap time display area
     let Interval;
+    let lapCounter = 1;
 
     const startTimer = () => {
         tens++;
@@ -34,22 +37,45 @@ window.onload = function() {
             seconds = '0';
             appendSeconds.innerHTML = '0' + 0;
         }
+        // Check if it's time for a new lap
+        if (tens === 0 && seconds % 10 === 0) {
+            const lapTime = `${minutes}:${seconds}:${tens}`;
+            const lapTimeElement = document.createElement('p');
+            lapTimeElement.textContent = `Lap ${lapCounter}: ${lapTime}`;
+            lapTimesContainer.appendChild(lapTimeElement);
+            lapCounter++;
+        }
     };
 
+    
     startBtn.onclick = () => {
-        clearInterval(Interval)
-        Interval = setInterval(startTimer,10);
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
     };
+
     stopBtn.onclick = () => {
-        clearInterval(Interval)
+        clearInterval(Interval);
     };
+
     resetBtn.onclick = () => {
-        clearInterval(Interval)
-        tens = '00'
-        seconds='00'
-        minutes='00'
+        clearInterval(Interval);
+        tens = '00';
+        seconds = '00';
+        minutes = '00';
         appendTens.innerHTML = tens;
         appendSeconds.innerHTML = seconds;
         appendMinutes.innerHTML = minutes;
-    }
+        lapCounter = 1;
+        lapTimesContainer.innerHTML = ''; // Clear lap times
+    };
+
+    lapBtn.onclick = () => {
+        if (Interval) {
+            const lapTime = `${minutes}:${seconds}:${tens}`;
+            const lapTimeElement = document.createElement('p');
+            lapTimeElement.textContent = `Lap ${lapCounter}: ${lapTime}`;
+            lapTimesContainer.appendChild(lapTimeElement);
+            lapCounter++;
+        }
+    };
 };
